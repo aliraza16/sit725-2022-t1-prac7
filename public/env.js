ce49897d3e3987f506bcc6c24436018adfd1e2d4
äkhowaja <akhowaja@deakin.edu.au>
2022-04-30 21:05:51 +1000

    prac 7

---
+++ public/env.js
@@ -0,0 +1,28 @@
+const testButtonFunction=()=>{
+  alert('Thank you for clicking')
+}
+
+// connect to the socket
+
+let socket = io();
+
+
+socket.on('number', (msg) => {
+    console.log('Random number: ' + msg);
+    $('#socket_output').html('Random number: ' + msg);
+})
+
+console.log('test')
+$(document).ready(function(){
+  console.log('Ready')
+
+  //bind the button
+  $('#testButton').click(testButtonFunction)
+
+  //test get call
+  $.get('/test?user_name="Fantastic User"',(result)=>{
+    console.log(result)
+  })
+
+
+})
